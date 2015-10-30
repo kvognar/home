31f642771f967f5f44a3cdf501e08f949030ac44
Kevin Vognar <kvognar@gmail.com>
2015-10-30 15:03:01 -0700

    No more coffee

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -1,3 +1,11 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$('#image-form').on('submit', submitImage);
+
+submitImage = function(event) {
+  event.preventDefault();
+
+}
