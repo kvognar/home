713b3dad0d038ad65140c1d6b0917db70b43501f
Kevin Vognar <kvognar@gmail.com>
2015-10-30 15:03:34 -0700

    Started asynchronous image uploading

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -1,11 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-$('#image-form').on('submit', submitImage);
+$('#image-form').on('change', submitImage);
 
-submitImage = function(event) {
-  event.preventDefault();
+var submitImage = function() {
+    $.ajax('/days', {
+        contents: {
+            photo: $('#image-form').val()
+        }
+    })
 
 }
