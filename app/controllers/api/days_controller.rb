e5e01d338d7fd3df16229cffcaf1216795dd5636
Kevin Vognar <kvognar@gmail.com>
2015-10-30 21:56:30 -0700

    migration api complete with comments and categories

--- app/controllers/api/days_controller.rb
+++ app/controllers/api/days_controller.rb
@@ -4,15 +4,11 @@ class Api::DaysController < ApplicationController
   def migrate
     Day.transaction do
 
-      @day = Day.new(migration_params)
+      @day = Day.new(day_params)
       if @day.save
-        puts "HEY LOOK AT THIS LOOK LOOK"
-        if tag_params.present?
-          tag_params[:tags].each do |tag|
-            puts tag
-            @day.tags << Tag.find_or_create_by(name: tag, tag_type: 'tag')
-          end
-        end
+        add_tags
+        add_categories
+        add_comments
         @photo = Photo.new(photo: photo_params[:photo], day: @day, is_canonical: true)
         if @photo.save
           render json: "hurrah!"
@@ -29,7 +25,7 @@ class Api::DaysController < ApplicationController
   private
 
 
-  def migration_params
+  def day_params
     params.require(:day).permit(:title,
                                 :number,
                                 :photo,
@@ -46,7 +42,39 @@ class Api::DaysController < ApplicationController
     params.require(:day).permit(tags: [])
   end
 
+  def category_params
+    params.require(:day).permit(categories: [])
+  end
+
   def photo_params
     params.require(:photo).permit(:photo)
+  end
+
+  def comment_params
+    params.require(:day).permit(comments: [:id, :author, :author_email, :body, :date, :parent_id])
+  end
+
+  def add_tags
+    if tag_params.present?
+      tag_params[:tags].each do |tag|
+        @day.tags << Tag.find_or_create_by(name: tag, tag_type: 'tag')
+      end
+    end
+  end
+
+  def add_categories
+    if category_params.present?
+      category_params[:categories].each do |category|
+        @day.categories << Tag.find_or_create_by(name: category, tag_type: 'category')
+      end
+    end
+  end
+
+  def add_comments
+    if comment_params.present?
+      comment_params[:comments].each_value do |comment|
+        @day.comments.create(comment)
+      end
+    end
   end
 end
