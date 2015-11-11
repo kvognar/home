8cb8092a824f3db60884f17c670e6d49e3bfef80
Kevin Vognar <kvognar@gmail.com>
2015-11-11 09:55:59 -0800

    add tags when posting

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -6,15 +6,19 @@ class DaysController < ApplicationController
   
   def create
     @day = Day.new(day_params)
-    if @day.save
-      if params[:photo]
-        photo = Photo.find(params[:photo][:id])
-        photo.update_attributes(day_id: @day.id, is_canonical: true)
+    Day.transaction do
+      if @day.save
+        add_and_remove_tags
+        # fail
+        if params[:photo]
+          photo = Photo.find(params[:photo][:id])
+          photo.update_attributes(day_id: @day.id, is_canonical: true)
+        end
+        redirect_to @day
+      else
+        flash.now[:errors] = @day.errors.full_messages
+        render :new
       end
-      redirect_to @day
-    else
-      flash.now[:errors] = @day.errors.full_messages
-      render :new
     end
   end
   
@@ -27,7 +31,39 @@ class DaysController < ApplicationController
   end
   
   private
-  
+
+  def add_and_remove_tags
+    ['people', 'categories'].each do |tag_type|
+      add_tags(tag_type)
+      remove_tags(tag_type)
+    end
+  end
+
+  def add_tags(tag_type)
+    if tag_params(tag_type).present?
+      tag_params(tag_type).each do |tag_name|
+        tag = Tag.find_or_create_by(tag_type: tag_type, name: tag_name)
+        @day.send(tag_type) << tag unless @day.send(tag_type).include? tag
+      end
+    end
+  end
+
+  def remove_tags(tag_type)
+      tags_to_keep = Tag.where(tag_type: tag_type, name: tag_params(tag_type))
+      tags_to_delete = @day.send(tag_type) - tags_to_keep
+      @day.tags.delete(tags_to_delete)
+  end
+
+
+  def tag_params(tag_type)
+    tag_string = params.require(:day)[tag_type]
+    if tag_string.present?
+      tag_string.split(',').map(&:strip)
+    else
+      []
+    end
+  end
+
   def day_params
     params.require(:day).permit(:title,
                                 :number,
