910e6c8071517060ce155dd7f323964e3bb91024
Kevin Vognar <kevin.vognar@workday.com>
2016-04-07 16:20:58 -0700

    comment specs

---
+++ spec/factories/comment_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :comment do
+    day
+    author 'Umbrella Man'
+    author_email 'good.morning@umbrellaman.com'
+    body 'Good morning.'
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,5 +16,33 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of :day }
+    it { should validate_presence_of :body }
+  end
+  context 'associations' do
+    it { should belong_to :day }
+    it { should belong_to :parent }
+    it { should have_many :children }
+  end
+  context 'scopes' do
+    let!(:top_level_comment) { create(:comment, approved: true) }
+    let!(:child_comment)     { create(:comment, parent: top_level_comment, approved: false) }
+    describe '#top_level' do
+      it 'returns comments with no parent comment' do
+        expect(Comment.top_level).to match_array([top_level_comment])
+      end
+    end
+    describe '#approved' do
+      it 'returns only approved comments' do
+        expect(Comment.approved).to match_array([top_level_comment])
+      end
+    end
+    describe '#unapproved' do
+      it 'returns only unapproved comments' do
+        expect(Comment.unapproved).to match_array([child_comment])
+      end
+    end
+
+  end
 end
