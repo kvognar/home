cc6610921772a536f09e7770d1ced8da968bf999
Kevin Vognar <kevin.vognar@workday.com>
2016-04-12 09:59:37 -0700

    days controller specs

---
+++ spec/controllers/days_controller_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe DaysController do
+  let!(:day_1) { create(:day) }
+  let!(:day_2) { create(:day) }
+  let!(:user) { create(:user, is_admin: true) }
+
+  describe '#new' do
+    it 'renders a new day page with an initialized day' do
+      controller.sign_in!(user)
+      get(:new)
+      expect(assigns(:day).number).to eq 3
+      expect(response).to render_template :new
+    end
+    it 'redirects to index if not signed in' do
+      get(:new)
+      expect(response).to redirect_to days_url
+    end
+  end
+
+  describe '#edit' do
+    it 'renders the edit page' do
+      controller.sign_in!(user)
+      get(:edit, id: day_1.slug)
+      expect(assigns(:day)).to eq day_1
+      expect(response).to render_template :edit
+    end
+    it 'redirects to index if not signed in' do
+      get(:edit, id: day_1.slug)
+      expect(response).to redirect_to days_url
+    end
+  end
+
+  describe '#show' do
+    it 'renders the show page' do
+      get(:show, id: day_1.slug)
+      expect(assigns(:day)).to eq day_1
+      expect(response).to render_template :show
+    end
+  end
+
+  describe '#index' do
+    it 'renders the index' do
+      get(:index)
+      expect(assigns(:days)).to eq [day_2, day_1]
+      expect(response).to render_template :index
+    end
+    it 'paginates correctly' do
+      ten_days = (1..10).map { create(:day) }
+      get(:index)
+      expect(assigns(:days)).to eq ten_days.reverse
+      get(:index, page: 2)
+      expect(assigns(:days)).to eq [day_2, day_1]
+    end
+  end
+
+  describe '#calendar' do
+    it 'renders all of the days???' do
+      get(:calendar)
+      expect(assigns(:days)).to eq Day.all
+      expect(response).to render_template :calendar
+    end
+  end
+
+  describe '#feed' do
+    it 'renders an rss feed' do
+      get(:feed, format: :rss)
+      expect(assigns(:days)).to eq [day_2, day_1]
+      expect(response).to render_template :feed
+    end
+  end
+end
