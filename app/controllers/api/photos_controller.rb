5b8b6a2023b03e7557c5d92c631c8dd2f8c49205
Kevin Vognar <kvognar@gmail.com>
2016-04-23 12:41:23 -0700

    uploaded photos are saved to draft

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,12 +1,16 @@
 class Api::PhotosController < ApplicationController
   before_action :require_admin!
   def create
-    @photo = Photo.new(photo: params[:file])
+    @photo = Photo.new(photo_params.merge(photo: params[:file]))
     if @photo.save
       render json: { id: @photo.id, url: @photo.photo.url(:medium) }, status: :ok
       puts "Photo saved!"
     else
       render json: { errors: @photo.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def photo_params
+    params.require(:photo).permit(:day_id, :is_canonical)
   end
 end
