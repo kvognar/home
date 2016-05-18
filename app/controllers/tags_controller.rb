ddd205317a6eec3e5a468f0920b19a3ab4f6b42a
Kevin Vognar <kvognar@gmail.com>
2016-05-17 18:50:15 -0700

    rough people page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,8 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
     @days = @tag.days.includes(:photo_of_the_day).paginate(page: params[:page])
   end
+
+  def people
+    @people = Tag.people.includes(:photos).group_by { |tag| tag.name.first }
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -12,9 +12,14 @@
 class Tag < ActiveRecord::Base
   has_many :taggings, dependent: :destroy
   has_many :days, through: :taggings, source: :day
+  has_many  :photos, -> { limit 1 }, through: :days, source: :photo_of_the_day
 
   scope :people, -> { where tag_type: 'people' }
   scope :categories, -> { where tag_type: 'categories' }
 
   default_scope { order(:name) }
+
+  def preview_photo
+    photos.first
+  end
 end
