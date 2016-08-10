d00fe9dc8a5d00fec4bd4ee8462a6c3f8b160546
Kevin Vognar <kevin.vognar@workday.com>
2016-08-10 08:02:45 -0700

    added RSS link

--- app/assets/javascripts/mu.js
+++ app/assets/javascripts/mu.js
@@ -1,1 +1,8 @@
 µ = {};
+
+$(document).on('page:change', function() {
+    $('#rss').on('click', function(event) {
+        event.preventDefault();
+        alert('To subscribe to my RSS feed, add this to your feed reader:\nhttp://kevinvognar.com/feed.rss')
+    });
+});
