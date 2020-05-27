090006d6ef424dc43419d6e6eab113c427aa0382
Kevin Vognar <kvognar@gmail.com>
2020-05-27 13:56:03 -0700

    removed the login specs from users yes I should make new specs but... later

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,23 +18,9 @@ describe User, type: :model do
   describe 'validations' do
     subject { create(:user) }
     it { should validate_presence_of :name }
-    it { should validate_presence_of :session_token }
     it { should validate_uniqueness_of :name }
-    it { should validate_uniqueness_of :session_token }
   end
-
-
-  it 'generates a session token and password hash on creation' do
-    user = User.create!(name: 'Mr. Midas', password: 'do not forget what you have done')
-    expect(user.session_token).to be_present
-    expect(user.password_hash).to be_present
-    expect(User.first.is_password?('do not forget what you have done')).to be true
-  end
-
-  it 'ensures a session token' do
-    expect(User.new.session_token).to be_present
-  end
-
+  
   it "knows if it's an admin" do
     admin = create(:user, is_admin: true)
     non_admin = create(:user)
