bf617cccfde5385c03f3c0e9f9b6cb8e4a22087e
Kevin Vognar <kvognar@gmail.com>
2021-12-29 21:09:53 -0800

    sort by medium and then by pips

--- app/services/year_in_review_helper.rb
+++ app/services/year_in_review_helper.rb
@@ -10,7 +10,9 @@ class YearInReviewHelper
 
   def data
     {
-        months: months
+        months:                 months,
+        most_thought_provoking: most_thought_provoking,
+        medium_stats:           medium_stats
     }
   end
 
@@ -24,14 +26,14 @@ class YearInReviewHelper
       sessions         = @sessions_by_month[month]
       sessions_by_work = sessions.group_by { |session| session.media_work }
 
-      sessions_by_work.values.sort_by { |s| s.length }.each do |work_sessions|
+      sessions_by_work.values.sort_by { |s| [s.first.media_work.medium, s.length] }.each do |work_sessions|
         work    = work_sessions.first.media_work
         summary = {
-            work:  work,
+            work:   work,
             medium: work.medium,
-            title: work.title,
-            count: work_sessions.length,
-            days:  work_sessions.map do |s|
+            title:  work.title,
+            count:  work_sessions.length,
+            days:   work_sessions.map do |s|
 
               icon = if s == s.media_consumption.media_sessions.last && s.media_consumption.finished?
                 '✔'
@@ -47,11 +49,11 @@ class YearInReviewHelper
 
             end
         }
-        if work.perennial?
+        # if work.perennial?
           evergreens << summary
-        else
-          deciduous << summary
-        end
+        # else
+        #   deciduous << summary
+        # end
       end
       months << {
           month: MONTHS[month - 1],
@@ -59,5 +61,13 @@ class YearInReviewHelper
       }
     end
     months
+  end
+
+  def most_thought_provoking
+    @sessions.group_by(&:media_work).values.sort_by { |sessions| sessions.map { |session| ((session.text || '') + (session.spoiler_text || '')).length } }.last.last.media_work
+  end
+
+  def medium_stats
+    @sessions.map { |i| i.media_work }.uniq.group_by(&:medium).map { |k, v| [k, v.count] }.to_h
   end
 end
