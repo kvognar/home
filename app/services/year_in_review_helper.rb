42e4f0cd584e7fcd7e899fd5accdab09439c774b
Kevin Vognar <kvognar@gmail.com>
2021-12-29 18:37:58 -0800

    off-by-one month fix

--- app/services/year_in_review_helper.rb
+++ app/services/year_in_review_helper.rb
@@ -39,7 +39,7 @@ class YearInReviewHelper
         end
       end
       months << {
-          month: MONTHS[month],
+          month: MONTHS[month + 1],
           works: deciduous + evergreens
       }
     end
